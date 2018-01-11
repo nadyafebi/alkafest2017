ba1a33d72788627ac06c69699f261a532d833cd8
Nadya Febiana Djojosantoso <nadyafebiana@gmail.com>
2018-01-11 15:28:36 +0700

    Add gulpfile.js and default layout

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+/*
+  Modules
+*/
+const browser = require('browser-sync');
+const gulp    = require('gulp');
+const panini  = require('panini');
+const sass    = require('gulp-sass');
+
+/*
+  Tasks
+*/
+
+gulp.task('build', gulp.parallel(compile, style));
+
+gulp.task('default', gulp.series('build', serve, watch));
+
+/*
+  Functions
+*/
+
+function compile() {
+  return gulp.src('./panini/pages/**/*.html')
+             .pipe(panini({
+               root: './panini/pages/',
+               layouts: './panini/layouts/',
+               partials: './panini/partials'
+             }))
+             .pipe(gulp.dest('./dist'));
+}
+
+function style() {
+  return gulp.src('./scss/**/*.scss')
+             .pipe(sass({
+               includePaths: './node_modules/foundation-sites/scss'
+             }).on('error', sass.logError))
+             .pipe(gulp.dest('./dist/css'));
+}
+
+function reset(done) {
+  panini.refresh();
+  done();
+}
+
+function serve(done) {
+  browser.init({
+    server: './dist',
+    port: 8000
+  });
+  done();
+}
+
+function watch() {
+  gulp.watch('./panini/pages/**/*.html').on('all', gulp.series(compile, browser.reload));
+  gulp.watch('./panini/{layouts,partials}/**/*.html').on('all', gulp.series(reset, compile, browser.reload));
+  gulp.watch('./sass/**/*.scss').on('all', gulp.series(style, browser.reload));
+}
