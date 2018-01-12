690ee5d8b8fe49d8e02aebea8b17ef2126fe92f6
Nadya Febiana Djojosantoso <nadyafebiana@gmail.com>
2018-01-12 22:05:39 +0700

    Add submit form function

--- dist/js/index.js
+++ dist/js/index.js
@@ -3,3 +3,33 @@ function start() {
     $('#nav-arrow').slideDown();
   });
 }
+
+function submitForm() {
+  var name = document.getElementById("nameBox").value;
+  var email = document.getElementById("emailBox").value;
+
+  $("#waitModal").foundation('open');
+
+  $.ajax({
+    type: "POST",
+    url: "register.php",
+    dataType: "html",
+    data: {
+      userName: name,
+      userEmail: email
+    },
+    success: function(response) {
+      console.log(response);
+      if (response == "SOLDOUT")
+      {
+        $("#soldModal").foundation('open');
+      }
+      else {
+        $("#successModal").foundation('open');
+      }
+    },
+    error: function(response) {
+      alert("Terjadi error pada website. Cek koneksi anda atau contact panitia Alkafest.");
+    }
+  });
+}
